fb3789b8cb848fccaeb57b0a9ed11f882ca65217
maoyaY <1335679005@qq.com>
2019-02-25 15:55:09 +0800

    到底部加载更多

--- src/api/api.js
+++ src/api/api.js
@@ -19,9 +19,12 @@ const get_medical_user = api2+'user/get_medical_user';//获取全部体检者
 const get_my_medical_user = api2+'user/get_my_medical_user';//获取我的体检者
 const add_my_medical_user = api2+'user/add_my_medical_user';//新增我的体检者
 const get_user_info = api2+'user/get_user_info';//个人信息
+const get_my_package = api2+'card/get_my_package';//获取卡包
+const add_to_my_package = api2 +'card/add_to_my_package';//兑换
+
 //评价
 module.exports = {
   login,get_medical_list,get_medical_message,get_science_list,get_science_message,get_medical_user,
-    get_my_medical_user,add_my_medical_user,get_loadpic_list,get_user_info
+    get_my_medical_user,add_my_medical_user,get_loadpic_list,get_user_info,get_my_package,add_to_my_package
 
 }
