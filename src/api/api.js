5310edd357736440de8f310f8a0ab9c469d0929f
maoyaY <1335679005@qq.com>
2019-02-26 16:04:50 +0800

    购物车接口

--- src/api/api.js
+++ src/api/api.js
@@ -22,7 +22,9 @@ const get_user_info = api2+'user/get_user_info';//个人信息
 const get_my_package = api2+'card/get_my_package';//获取卡包
 const add_to_my_package = api2 +'card/add_to_my_package';//兑换\
 const make_order = api2 + 'order/make_order';//创建订单
-
+const make_cart = api2+ 'order/make_cart';//添加购物车
+const get_cart = api2 + 'order/get_cart';//获取购物车
+const delete_cart = api2 + 'order/delete_cart';//删除购物车
 const get_location = api2 +'medical/get_location';//获取地址
 
 
@@ -30,6 +32,6 @@ const get_location = api2 +'medical/get_location';//获取地址
 module.exports = {
   login,get_medical_list,get_medical_message,get_science_list,get_science_message,get_medical_user,
     get_my_medical_user,add_my_medical_user,get_loadpic_list,get_user_info,get_my_package,add_to_my_package,
-    get_location,make_order
+    get_location,make_order,make_cart,get_cart,delete_cart
 
 }
