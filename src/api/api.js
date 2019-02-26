3ccd93bc1a203b2d9e86d1841599868119fcfa26
maoyaY <1335679005@qq.com>
2019-02-26 17:05:06 +0800

    订单详情

--- src/api/api.js
+++ src/api/api.js
@@ -26,12 +26,15 @@ const make_cart = api2+ 'order/make_cart';//添加购物车
 const get_cart = api2 + 'order/get_cart';//获取购物车
 const delete_cart = api2 + 'order/delete_cart';//删除购物车
 const get_location = api2 +'medical/get_location';//获取地址
-
-
+const get_order_count_user = api2 +'order/get_order_count_user';//订单数量
+const get_my_order = api2 +'order/get_my_order';//获取我的订单
+const get_order_message = api2 +'order/get_order_message';//获取订单详情
+const update_order = api2 +'order/update_order';//更新订单
 //评价
 module.exports = {
   login,get_medical_list,get_medical_message,get_science_list,get_science_message,get_medical_user,
     get_my_medical_user,add_my_medical_user,get_loadpic_list,get_user_info,get_my_package,add_to_my_package,
-    get_location,make_order,make_cart,get_cart,delete_cart
+    get_location,make_order,make_cart,get_cart,delete_cart,get_order_count_user,get_my_order,get_order_message,
+    update_order
 
 }
