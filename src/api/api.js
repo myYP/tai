01782462c5b1df4fcb44700792211cb816a93fb8
maoyaY <1335679005@qq.com>
2019-02-26 13:44:05 +0800

    购物车

--- src/api/api.js
+++ src/api/api.js
@@ -20,7 +20,8 @@ const get_my_medical_user = api2+'user/get_my_medical_user';//获取我的体检
 const add_my_medical_user = api2+'user/add_my_medical_user';//新增我的体检者
 const get_user_info = api2+'user/get_user_info';//个人信息
 const get_my_package = api2+'card/get_my_package';//获取卡包
-const add_to_my_package = api2 +'card/add_to_my_package';//兑换
+const add_to_my_package = api2 +'card/add_to_my_package';//兑换\
+const make_order = api2 + 'order/make_order';//创建订单
 
 const get_location = api2 +'medical/get_location';//获取地址
 
@@ -29,6 +30,6 @@ const get_location = api2 +'medical/get_location';//获取地址
 module.exports = {
   login,get_medical_list,get_medical_message,get_science_list,get_science_message,get_medical_user,
     get_my_medical_user,add_my_medical_user,get_loadpic_list,get_user_info,get_my_package,add_to_my_package,
-    get_location
+    get_location,make_order
 
 }
